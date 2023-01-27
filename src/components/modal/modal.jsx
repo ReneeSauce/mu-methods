ac1ef25b224fc162cc56427f7c9f6bb7e53538a2
nuriya <nuriya_a@yahoo.com>
2023-01-27 17:25:34 -0500

    feat: wip,added desktop position, need to fix footer

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import { Button } from "../button";
 import styled from "styled-components";
 
-const ModalComponent=styled.div`
+const ModalComponent = styled.div`
 box-sizing: border-box;
   justify-content: center;
-  align-items: flex-end;
+  align-items: center;
   position: fixed;
   bottom: 0;
   left: 0;
@@ -18,8 +18,12 @@ box-sizing: border-box;
   transition: visibility 0s ease-in-out 0.4s, opacity 0.4s ease-in-out;
   margin: 0;
   padding: 0;
-  color:white;`
-  const ModalComponent1=styled.div`
+  color:white;
+  
+  @media (max-width: 1000px) {
+    align-items: flex-end;}
+  `;
+const ModalComponent1 = styled.div`
   box-sizing: border-box;
   justify-content: center;
   align-items: flex-end;
@@ -32,113 +36,125 @@ box-sizing: border-box;
   display: flex;
   background-color: rgba(0, 0, 0, 0.5);
   z-index: 999;
-  transition: transform 0.3s ease,visibility 0s ease-in-out 0.4s, opacity 0.4s ease-in-out;
+  transition: transform 0.3s ease, visibility 0s ease-in-out 0.4s,
+    opacity 0.4s ease-in-out;
   margin: 0;
-  padding: 0;`
+  padding: 0;
+`;
 const ModalContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 375px;
-  min-height:465px;
+  min-height: 465px;
   background: #2d2d41;
   border-radius: 16px 16px 0px 0px;
-  align-items:center;
+  align-items: center;
   padding: 16px 16px 24px;
   box-sizing: border-box;
-  position:relative;
-  
+  position: relative;
+
+  @media (min-width: 1000px) {
+    border-radius: 16px 16px 16px 16px;}
 `;
 const ModalHeader = styled.div`
+  display: flex;
+`;
+const ModalTitle = styled.p`
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 120%;
+  /* identical to box height, or 12px */
+  text-align: center;
+  text-transform: uppercase;
+  font-feature-settings: "pnum" on, "lnum" on;
 
-outline:1px solid red;
-display:flex;
+  /* Dark/Content/Secondary */
 
-`
-const ModalTitle= styled.p`
-font-family: 'DM Sans';
-font-style: normal;
-font-weight: 400;
-font-size: 10px;
-line-height: 120%;
-/* identical to box height, or 12px */
-text-align: center;
-text-transform: uppercase;
-font-feature-settings: 'pnum' on, 'lnum' on;
+  color: rgba(246, 246, 246, 0.7);
+  margin: 32px 0 16px;
+`;
+const ModalBody = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-/* Dark/Content/Secondary */
+const ModalText = styled.h3`
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 120%;
+  /* identical to box height, or 22px */
 
-color: rgba(246, 246, 246, 0.7);
-margin:32px 0 16px;
-`
-const ModalBody=styled.div`
-display:flex;
-flex-direction:column;
-;
-outline:1px solid red;
-height:100%`
+  text-align: center;
+  font-feature-settings: "pnum" on, "lnum" on;
 
-const ModalText=styled.h3`
+  /* Dark/Content/Primary */
 
-font-family: 'DM Sans';
-font-style: normal;
-font-weight: 700;
-font-size: 18px;
-line-height: 120%;
-/* identical to box height, or 22px */
+  color: #f6f6f6;
+  margin-bottom: 32px;
+`;
+const ModalNotificationBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 111px;
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 120%;
+  /* or 22px */
 
-text-align: center;
-font-feature-settings: 'pnum' on, 'lnum' on;
+  text-align: center;
+  font-feature-settings: "pnum" on, "lnum" on;
 
-/* Dark/Content/Primary */
+  /* Dark/Content/Primary */
 
-color: #F6F6F6;
-margin-bottom:32px;`
- const ModalNotificationBox=styled.div`
- display:flex;
- flex-direction:column;
- height:111px;
- `
-const ModalFooter=styled.div`
-display:flex;
-flex-direction:column;
-margin-top:57px;
+  color: #f6f6f6;
+  padding: 0 30px;
+`;
+const ModalFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 57px;
 
-
-outline: solid 1px yellow;
-`
-const ModalCloseButton=styled.div`
+  outline: solid 1px yellow;
+`;
+const ModalCloseButton = styled.div`
   box-sizing: border-box;
   margin: 0;
   padding: 0;
   position: absolute;
-  top: 40px;
-  right: 60px;
+  top: -26px;
+  right: 26px;
   width: 12px;
   height: 12px;
   background-color: transparent;
-  background-image: url(../assets/closeIcon.svg);
+  background-image: url(../../assets/closeIcon.svg);
   background-position: center;
   background-size: cover;
   border: none;
   cursor: pointer;
-  transition: opacity 0.3s;`
+  transition: opacity 0.3s;
+`;
 
-const Modal = ({header, title, text,children}) => {
+const Modal = ({ header, title, text, children, notifications, footer }) => {
   return (
-    
-    <ModalComponent> {children}
+    <ModalComponent>
+      {" "}
+      {children}
       <ModalContainer>
-      <ModalHeader>{header}</ModalHeader>
-      <ModalTitle>{title}</ModalTitle>
-      <ModalBody>
-        <ModalText>{text}</ModalText>
-        <ModalNotificationBox>{notifications}</ModalNotificationBox>
-      </ModalBody>
-      <ModalFooter>{footer}</ModalFooter>
-    
+        <ModalHeader>{header}</ModalHeader>
+        <ModalTitle>{title}</ModalTitle>
+        <ModalBody>
+          <ModalText>{text}</ModalText>
+          <ModalNotificationBox>{notifications}</ModalNotificationBox>
+        </ModalBody>
+        <ModalFooter><Button >Decline</Button><Button >Sign in</Button></ModalFooter>
       </ModalContainer>
     </ModalComponent>
   );
 };
 export default Modal;
- 
