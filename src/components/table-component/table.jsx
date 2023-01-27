bf525a37ddb6b0c835ff0e9b40744ef5f30c40fb
Kurtney Joseph <josephkurt18@gmail.com>
2023-01-27 17:41:42 -0500

    feat: table comp & added storybook

--- src/components/table-component/table.jsx
+++ src/components/table-component/table.jsx
@@ -2,24 +2,19 @@ import styled from "styled-components";
 
 const StyledCell = styled.div`
   width: 343px;
-  min-height: 84px;
-  margin-bottom: 8px;
+  height: 84px;
   background: rgba(246, 246, 246, 0.1);
-  border-radius: 8px;
 `;
 
 const StyledHeading = styled.p`
   font-size: 10px;
-  font-weight: 400;
   line-height: 12px;
-  letter-spacing: 0em;
   color: rgba(246, 246, 246, 0.7);
 `;
 
 const StyledAvatar = styled.img`
   width: 40px;
   height: 40px;
-  border-radius: 20px;
   margin: 22px 12px 0 12px;
 `;
 
@@ -34,11 +29,11 @@ const Header = ({ children }) => {
 };
 
 const Cell = ({ children }) => {
-  return <StyledCell>{children}</StyledCell>;
+  return <StyledCell className="rounded mb-2">{children}</StyledCell>;
 };
 
 const Avatar = ({ src, alt }) => {
-  return <StyledAvatar src={src} alt={alt}></StyledAvatar>;
+  return <StyledAvatar className="rounded-circle" src={src} alt={alt} />;
 };
 
 Table.Header = Header;
