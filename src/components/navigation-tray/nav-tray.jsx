5d9070f422fb93a8406c2812084927ff06a13821
kamal-ganiev <kamal.ganiev999@gmail.com>
2023-01-30 18:03:32 -0500

    refactor: change code for navigation tray

--- src/components/navigation-tray/nav-tray.jsx
+++ src/components/navigation-tray/nav-tray.jsx
@@ -6,9 +6,9 @@ import notificationIconActive from "../../assets/notification_active.svg";
 import homeIcon from "../../assets/home.svg";
 import homeIconActive from "../../assets/home_active.svg";
 import notificationSign from "../../assets/notification_sign.svg";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const StyledNavLink = styled(NavLink)`
+const NavGlobalStyle = css`
   width: 60px;
   height: 60px;
   background-position: center;
@@ -23,66 +23,62 @@ const StyledNavLink = styled(NavLink)`
   }
 `;
 
+const StyledProfileNavLink = styled(NavLink)`
+  ${NavGlobalStyle}
+  background-image: url("${profileIcon}");
+  &.active {
+    background-image: url("${profileIconActive}");
+  }
+`;
+
+const StyledHomeNavLink = styled(NavLink)`
+  ${NavGlobalStyle}
+  background-image: url("${homeIcon}");
+  &.active {
+    background-image: url("${homeIconActive}");
+  }
+`;
+
+const StyledNotificationNavLink = styled(NavLink)`
+  ${NavGlobalStyle}
+  background-image: url("${notificationIcon}");
+  &.active {
+    background-image: url("${notificationIconActive}");
+  }
+`;
+
 const StyledNavBar = styled.nav`
   width: 215px;
   height: 60px;
   background-color: white;
-  position: fixed;
   bottom: 3%;
   right: 50%;
   transform: translate(50%, 0);
 `;
 
-export const NavTray = () => {
-  let num = 0;
-
-  const navList = [
-    {
-      linkTo: "/login",
-      imageURL: `url('${profileIcon}')`,
-      imageActiveURL: `url('${profileIconActive}')`,
-      isNotification: false,
-    },
-    {
-      linkTo: "/",
-      imageURL: `url('${homeIcon}')`,
-      imageActiveURL: `url('${homeIconActive}')`,
-      isNotification: false,
-    },
-    {
-      linkTo: "/notification",
-      imageURL: `url('${notificationIcon}')`,
-      imageActiveURL: `url('${notificationIconActive}')`,
-      isNotification: true,
-    },
-  ];
-
+export const NavTray = ({ isNotification }) => {
   return (
     <StyledNavBar
-      className="nav d-flex justify-content-evenly align-items-center rounded-pill bg-white"
+      className="nav position-fixed d-flex justify-content-evenly align-items-center rounded-pill bg-white"
       style={{ "--bs-bg-opacity": 0.1 }}
     >
-      {navList.map((navLink) => {
-        return (
-          <StyledNavLink
-            key={(num += 1)}
-            className="nav-link rounded-circle"
-            style={{ backgroundImage: navLink.imageURL }}
-            to={navLink.linkTo}
-          >
-            {navLink.isNotification ? (
-              <img
-                className="position-absolute"
-                style={{ top: "13px", right: "13px" }}
-                alt="Notification Sign"
-                src={notificationSign}
-              />
-            ) : (
-              ""
-            )}
-          </StyledNavLink>
-        );
-      })}
+      <StyledProfileNavLink className="nav-link rounded-circle" to="/login" />
+      <StyledHomeNavLink className="nav-link rounded-circle" to="/" />
+      <StyledNotificationNavLink
+        className="nav-link rounded-circle"
+        to="/notification"
+      >
+        {isNotification ? (
+          <img
+            className="position-absolute"
+            style={{ top: "13px", right: "13px" }}
+            alt="Notification Sign"
+            src={notificationSign}
+          />
+        ) : (
+          ""
+        )}
+      </StyledNotificationNavLink>
     </StyledNavBar>
   );
 };
--- src/routes/notification.jsx
+++ src/routes/notification.jsx
@@ -1,10 +1,7 @@
-import { NavTray } from "../components/navigation-tray/nav-tray";
-
 export const Notification = () => {
   return (
     <div className="bg-alpha">
       <h1>Notification</h1>
-      <NavTray activeKey="notification" />
     </div>
   );
 };
