52abe6cc6718330fd86229189da22a26886f1c2f
Shaheen Hadadzadeh <shaheenhad@gmail.com>
2023-01-30 19:54:52 -0500

    feat: added opacity and shape options for pill

--- src/components/pill/pill.jsx
+++ src/components/pill/pill.jsx
@@ -2,6 +2,15 @@ import styled from "styled-components";
 import { useState, useEffect } from "react";
 import copyIcon from "../../assets/copy-icon.svg";
 
+/**
+ * Pill Component
+ * @param shape expects 'round' or 'square'
+ * @param isCopiable expects 'true' or 'false'
+ * @param text expects the text to be displayed in the pill
+ * @param opacity expects the pill background opacity '10' or '25'
+ * @param textOpacity expects the text opacity '100' or '75'
+ */
+
 const CopyBtn = styled.button`
   background-image: url(${copyIcon});
   background-position: center;
@@ -12,7 +21,7 @@ const CopyBtn = styled.button`
   background-color: transparent;
 `;
 
-export const Pill = ({ isCopiable, text }) => {
+export const Pill = ({ isCopiable, text, shape, opacity, textOpacity }) => {
   const [pillText, setPillText] = useState("none");
 
   useEffect(() => {
@@ -21,7 +30,13 @@ export const Pill = ({ isCopiable, text }) => {
 
   return (
     <div className="d-flex flex-row align-items-center">
-      <div className="badge text-bg-primary">{pillText}</div>
+      <div
+        className={`badge bg-white text-white text-opacity-${textOpacity} bg-opacity-${opacity} ${
+          shape === "round" ? "rounded-pill" : ""
+        }`}
+      >
+        {pillText}
+      </div>
       {isCopiable && (
         <CopyBtn
           onClick={() => {
--- src/components/pill/pill.stories.jsx
+++ src/components/pill/pill.stories.jsx
@@ -8,6 +8,19 @@ export default {
       options: ["true", "false"],
       control: { type: "radio" },
     },
+    text: { control: "text" },
+    shape: {
+      options: ["round", "square"],
+      control: { type: "radio" },
+    },
+    opacity: {
+      options: ["10", "25"],
+      control: { type: "radio" },
+    },
+    textOpacity: {
+      options: ["100", "75"],
+      control: { type: "radio" },
+    },
   },
 };
 
@@ -16,4 +29,7 @@ const Template = (args) => <Pill {...args} />;
 export const Variants = Template.bind({});
 Variants.args = {
   isCopiable: "false",
+  text: "text",
+  opacity: "25",
+  textOpacity: "100",
 };
