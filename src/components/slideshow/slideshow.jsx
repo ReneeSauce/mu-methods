0e83a5b95fcda560de16b600573c3e74a8690bb2
Jeremy M Rivera <fangman09@gmail.com>
2023-01-31 14:57:49 -0500

    style: styles complete before slider addition

--- src/components/slideshow/slideshow.jsx
+++ src/components/slideshow/slideshow.jsx
@@ -13,18 +13,22 @@ export const SlideShow = () => {
  "
     >
       <SlideShowLogo />
-      <SlideShowTitle className="mx-auto">
+
+      <SlideShowTitle className="mx-auto ">
         {" "}
-        Welcome to MU Methods{" "}
+        <span className="text-alpha">'--</span>Welcome{" "}
+        <span className="text-alpha">---</span>to MU Methods{" "}
       </SlideShowTitle>
+
       <SlideShowImage />
       <p> All your crypto accounts in one place</p>
+
       <ButtonContainer className="d-flex flex-column m-auto">
         <Button className="bg-alpha border-alpha text-white60 btn-lg">
           {" "}
           Log In{" "}
         </Button>
-        <Button className="text-alpha bg-primary btn-lg"> Sign Up </Button>
+        <Button className="text-alpha bg-primary btn-lg mt-3"> Sign Up </Button>
         <Button className="bg-alpha border-white mt-3 text-white60 btn-lg">
           Connect Existing Web3 wallet
         </Button>
--- src/components/slideshow/slideshow.styles.jsx
+++ src/components/slideshow/slideshow.styles.jsx
@@ -3,7 +3,11 @@ import muLogo from "../../assets/mu-methods.svg";
 import cubes from "../../assets/cubes.svg";
 
 export const SlideShowLogo = styled.div`
+  margin-top: 63px;
+  margin-right: 6px;
+  margin-bottom: 30px;
   width: 30px;
+
   height: 37px;
   background-image: url(${muLogo});
 `;
@@ -11,16 +15,20 @@ export const SlideShowLogo = styled.div`
 export const SlideShowTitle = styled.h1`
   max-width: 235px;
   height: 64px;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 32px;
   margin-bottom: 36px;
 `;
 
 export const SlideShowImage = styled.div`
   width: 375px;
   height: 222px;
-  margin-left: 7px;
+  margin-left: 17px;
 
   background-image: url(${cubes});
 `;
 export const ButtonContainer = styled.div`
   width: 343px;
+  padding-bottom: 84px;
 `;
