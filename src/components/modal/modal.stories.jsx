f9318703e6961cd2de33ae35a35da6b1efaf667f
nuriya <nuriya_a@yahoo.com>
2023-02-02 19:29:35 -0500

    refactor: wip storybook

--- src/components/modal/modal.stories.jsx
+++ src/components/modal/modal.stories.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import Modal from "./modal";
-import closeIcon from "../../assets/closeIcon.svg";
-import {
-  motion,
-  AnimatePresence,
-  useAnimation,
-  useMotionValue,
-  useTransform,
-} from "framer-motion";
-import useMediaQuery from "../../utils/useMediaQuery";
+import { Modal } from "./modal";
+
+
 export default {
   title: "Components/Modal",
   component: Modal,
@@ -17,31 +10,33 @@ export default {
     text: { control: "text" },
     notifications: { control: "text" },
     onClose: { action: "onClose" },
-    isLessThan1000: {
-      type: "number",
-      defaultValue: 1000,
+    isOpen: {
+      options: [true, false],
+      control: { type: "radio" },
     },
   },
 };
-// const Template = ({ ...args }) => <Modal {...args} />;
 
-// export const MobileModal = Template.bind({});
-// MobileModal.args = {
-//   title: "coinbase",
-//   text: "Transaction for 0.008 ETH sign request",
-//   notifications: "Type “carrot pizza” to sign your transaction",
-// };
+const Template = ({ ...args }) => <Modal {...args} />;
+export const ModalComponent = Template.bind({});
+ModalComponent.args = {
+  title: "coinbase",
+  text: "Transaction for 0.008 ETH sign request",
+  notifications: "Type “carrot pizza” to sign your transaction",
+  isOpen: true,
+  // onClose:???
+};
 
-export const Default = () => (
-
+/*
+export const Default = () => {
+return (
  <Modal>
  <Modal.Component>
 
-  
-  <Modal.Wrapper>
+    <Modal.Wrapper>
     <Modal.CloseButton
       type="button"
-      onClick={()=>console.log("onClose")}
+      onClick={closeModal}
       aria-label="Close modal"
       data-bs-dismiss="modal"
     ></Modal.CloseButton>
@@ -50,15 +45,15 @@ export const Default = () => (
       <Modal.Header />
       <Modal.Title>coinbase</Modal.Title>
       <Modal.Body>
-        {/* <ModalText>{text}</ModalText>
-        <ModalNotificationBox>{notifications}</ModalNotificationBox> */}
+       <ModalText>{text}</ModalText>
+        <ModalNotificationBox>{notifications}</ModalNotificationBox> 
       </Modal.Body>
     </Modal.Container>
     <Modal.Footer>
       <Modal.Button
         className="bg-dangerOpacity50"
         type="button"
-        onClick={()=>console.log("onClose")}
+        onClick={closeModal}
         data-bs-dismiss="modal"
         aria-label="Decline action or close modal"
       >
@@ -67,6 +62,7 @@ export const Default = () => (
     </Modal.Footer>
   </Modal.Wrapper>
 </Modal.Component>
-</Modal> 
+</Modal> )
   
-);
+      };
+      */
