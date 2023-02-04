4d0f6647d7e9335c6dd174b226ba276198b8b4e7
Kurtney Joseph <josephkurt18@gmail.com>
2023-02-04 13:45:59 -0500

    feat: add test scripts for table comp

---
+++ src/components/table/table.test.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { render, screen } from "@testing-library/react";
+
+import { Table } from "./table";
+
+// Table Comp Build Test Scripts Below: Table, Heading & Cell.
+
+describe("Component: Table", () => {
+  it("Renders table", () => {
+    render(<Table />);
+    expect(screen.getByRole("table")).toBeInTheDocument();
+  });
+
+  it("Renders table cell", () => {
+    render(<Table.Cell />);
+    expect(screen.getByRole("cell")).toBeInTheDocument();
+  });
+
+  it("Renders table heading", () => {
+    render(<Table.Header />);
+    expect(screen.getByRole("heading")).toBeInTheDocument();
+  });
+});
