fd035b91f0fbf43fa4abb296a3e0cb6f8c9e43a8
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-02-07 11:33:41 -0800

    feat: add user-info and seed-phrase sign-up steps

---
+++ src/routes/sign-up/steps/user-info.jsx
@@ -0,0 +1,35 @@
+import { Step } from "../../../components/step";
+import { Button } from "../../../components/button/button";
+
+//TO DO
+//need to import and add correct buttons when they are created
+//need to import and add corresponding form when it is created
+//needs logic to call modal
+//needs logic to disable/enable button - ?controlled by form validation?
+//need to identify props
+//QUESTIONS
+//where should click handlers go in the tree
+//is there a better way to structure or name header components
+//should title and subtitle be passed with props or written in component
+//should it be subTitle or subtitle
+//btnText or btntext
+
+export default function UserInfo(title, subTitle, btnText, onBackClick, onForwardClick) {
+  <Step.Container>
+    <Step.Header>
+      <Step.Header.Left>
+        {/* insert back button here with onClick={onBackClick}*/}
+      </Step.Header.Left>
+      <Step.Header.Center>
+        <Step.Header.Title title={title}></Step.Header.Title>
+        <Step.Header.SubTitle subtitle={subTitle}></Step.Header.SubTitle>
+      </Step.Header.Center>
+    </Step.Header>
+    <Step.Body>
+        {/* insert form comopnent here */}
+        </Step.Body>
+    <Step.Footer>
+      <Button onClick={onForwardClick}>{btnText}</Button>
+    </Step.Footer>
+  </Step.Container>;
+}
