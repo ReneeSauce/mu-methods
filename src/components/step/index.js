7d2bf5236d7ca99e84768a307565065de2d1827d
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-02-07 09:53:02 -0800

    feat: add index.js to step

---
+++ src/components/step/index.js
@@ -0,0 +1,6 @@
+import { container as Container } from "./container";
+import { header as Header } from "./header";
+import { body as Body } from "./body";
+import { footer as Footer } from "./footer";
+
+export const Step = { Container, Header, Body, Footer };
