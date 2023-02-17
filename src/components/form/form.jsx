8ae57712d1e19aa4523fdc53f9c1b872e819c272
Jake Bohn <jakoborion@gmail.com>
2023-02-16 19:57:14 -0500

    feat: adding react hook form and form component

---
+++ src/components/form/form.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+/**
+ * Form Component
+ * @author [Jake Bohn](https://github.com/JakobOrion)
+ */
+
+export default function Form({ defaultValues, children, onSubmit }) {
+  const methods = useForm({ defaultValues });
+  const { handleSubmit } = methods;
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {React.Children.map(children, (child) => {
+        return child.props.name
+          ? React.createElement(child.type, {
+              ...{
+                ...child.props,
+                register: methods.register,
+                key: child.props.name,
+              },
+            })
+          : child;
+      })}
+    </form>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export * from "./avatar";
 export * from "./button";
 export * from "./feed";
+export * from "./form";
 export * from "./modal";
 export * from "./notification";
 export * from "./pill";
