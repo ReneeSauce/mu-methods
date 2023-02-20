c563e63b396a9bad78aa2870e0a770a8d6e95ee5
Shaheen Hadadzadeh <shaheenhad@gmail.com>
2023-02-20 18:10:15 -0500

    fix: corrext pill sizing and add btn animation

--- src/components/pill/pill.jsx
+++ src/components/pill/pill.jsx
@@ -1,19 +1,22 @@
 import cx from "classnames";
+import { motion } from "framer-motion";
 import styled from "styled-components";
 import copyIcon from "../../assets/copy-icon.svg";
 
 /**
  * Pill Component
+ * @author [Shaheen Hadadzadeh](https://github.com/shaheenhad)
  * @param className optional classNames that will be passed to the pill
  * @param shape expects 'round' or 'square'
  * @param isCopiable expects 'true' or 'false'
  * @param text expects the text to be displayed in the pill
  * @param opacity expects the pill background opacity '10' or '20'
  * @param textOpacity expects the text opacity '100' or '70'
+ * @param size 'small' or 'large'
  */
 
 // TODO: Refactor to use common Button Component
-const CopyBtn = styled.button`
+const CopyBtn = styled(motion.button)`
   background-image: url(${copyIcon});
   background-position: center;
   background-repeat: no-repeat;
@@ -30,14 +33,15 @@ export const Pill = ({
   shape,
   opacity,
   textOpacity,
+  size,
 }) => {
   return (
     <div className="d-flex flex-row align-items-center">
       <div
         className={cx(
-          `badge bg-white bg-opacity-${opacity} text-opacity-${textOpacity} text-white fs-14px ${
+          `badge px-8px lh-sm bg-white bg-opacity-${opacity} text-opacity-${textOpacity} text-white fs-12px ${
             shape === "round" ? "rounded-pill" : ""
-          }`,
+          } ${size === "large" ? "py-4px" : "py-2px"}`,
           className
         )}
       >
@@ -46,10 +50,10 @@ export const Pill = ({
       {isCopiable && (
         <CopyBtn
           onClick={() => {
-            // TODO: Add a visual indicator that something was copied
             navigator.clipboard.writeText(text);
           }}
-          className="ms-2"
+          className="ms-8px"
+          whileTap={{ scale: 0.75 }}
         ></CopyBtn>
       )}
     </div>
