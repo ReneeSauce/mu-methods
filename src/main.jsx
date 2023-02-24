b8cb6cc0892b75ad6952917709e9fab2374a2d67
Julia <JLHartsek@gmail.com>
2023-02-24 11:07:37 -0500

    feat: added account header

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home, Login, Profile } from "./routes";
+import { Account, Home, Login, Profile } from "./routes";
 import "./theme/defaults.scss";
 
 const router = createBrowserRouter([
@@ -25,6 +25,20 @@ const router = createBrowserRouter([
         isCopiable={true}
         email="emailexample@gmail.com"
         id="ID: JU1s32"
+      />
+    ),
+  },
+  {
+    path: "/account",
+    element: (
+      <Account
+        src="https://images.unsplash.com/photo-1615361200141-f45040f367be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+        alt="sushi"
+        name="MyMetaMaskAkk"
+        wallet="Metamask"
+        permissions="F"
+        pillText="0x6b...7776"
+        isCopiable={true}
       />
     ),
   },
