b9306a16adbbae9f664c25a8d9dd2a870e41f40c
kamal-ganiev <kamal.ganiev999@gmail.com>
2023-03-02 14:34:30 -0500

    refactor: make changes on icons for navigation tray

--- src/components/icons/profile.jsx
+++ src/components/icons/profile.jsx
@@ -1,4 +1,4 @@
-const Profile = ({ className, width = "16", height = "16" }) => {
+const ProfileIcon = ({ className, width = "16", height = "16", active }) => {
   return (
     <svg
       width={width}
@@ -8,10 +8,16 @@ const Profile = ({ className, width = "16", height = "16" }) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="11" cy="11" r="11" fill="#F6F6F6" fillOpacity="0.7" />
+      <circle
+        cx="11"
+        cy="11"
+        r="11"
+        fill="#F6F6F6"
+        fillOpacity={active ? "1" : "0.7"}
+      />
       <mask
         id="mask0_1508_1842"
-        style={{ "mask-type": "alpha" }}
+        style={{ maskType: "alpha" }}
         maskUnits="userSpaceOnUse"
         x="2"
         y="2"
@@ -25,11 +31,18 @@ const Profile = ({ className, width = "16", height = "16" }) => {
         <path
           d="M4 19C4 15.6863 6.68629 13 10 13H12C15.3137 13 18 15.6863 18 19V20H4V19Z"
           fill="#F6F6F6"
-          fillOpacity="0.7"
+          fillOpacity={active ? "1" : "0.7"}
         />
-        <circle cx="11" cy="8" r="3" fill="#F6F6F6" fillOpacity="0.7" />
+        <circle
+          cx="11"
+          cy="8"
+          r="3"
+          fill="#F6F6F6"
+          fillOpacity={active ? "1" : "0.7"}
+        />
       </g>
     </svg>
   );
 };
-export default Profile;
+
+export default ProfileIcon;
