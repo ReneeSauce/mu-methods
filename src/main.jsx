07dde868ccf93b4bd35ec3e345f9c97016876a33
nuriya <nuriya_a@yahoo.com>
2023-03-08 14:27:53 -0500

    refactor: changed notification route in nav tray

--- src/main.jsx
+++ src/main.jsx
@@ -20,53 +20,6 @@ import "./theme/defaults.scss";
 
 const isLoggedIn = true;
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/addaccount",
-    element: <AddAccount />,
-  },
-  {
-    path: "/profile",
-    element: (
-      <Profile
-        src="https://images.unsplash.com/photo-1615361200141-f45040f367be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-        alt="sushi"
-        name="Jugal Mu"
-        pillText="0x6b...7776"
-        isCopiable={true}
-        email="emailexample@gmail.com"
-        id="ID: JU1s32"
-      />
-    ),
-  },
-  {
-    path: "/account",
-    element: (
-      <Account
-        src="https://images.unsplash.com/photo-1615361200141-f45040f367be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-        alt="sushi"
-        name="MyMetaMaskAkk"
-        wallet="Metamask"
-        permissions="F"
-        pillText="0x6b...7776"
-        isCopiable={true}
-      />
-    ),
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <RouterProvider router={router} /> */}
@@ -117,11 +70,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             },
             {
               role: "home",
-              to: "/",
+              to: "/signup",
               Icon: HomeIcon,
             },
             {
-              to: "/account",
+              to: "/notifications",
               Icon: NotificationIcon,
               isNotification: true,
             },
