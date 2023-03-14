d1cd164f7559cf6321644bf43b9bea52500b7df0
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-03-14 13:21:21 -0700

    style: remove commented out part in dashboard

--- src/routes/dashboard/dashboard.jsx
+++ src/routes/dashboard/dashboard.jsx
@@ -150,43 +150,6 @@ export const Dashboard = () => {
                 )}
               </div>
             ))}
-
-            {/* <Table.Cell>
-              <Wallet
-                alt="wallet avatar"
-                id={"1234567"}
-                name="MyMetamaskAkk"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5WkTt5462C95lYurbWL1ic0jtmW9UOGxlB-cFd3A3Dm8U8UFTELI-jNqmx-c-HlGd8NA&usqp=CAU"
-                wallet="METAMASK"
-                currency="ETH 0.80"
-                amount="$ 1,000"
-              />
-            </Table.Cell>
-
-            <div className="mt-32px"></div>
-            <Table.Header>Linked Accounts</Table.Header>
-            <Table.Cell>
-              <Wallet
-                alt="wallet avatar"
-                id="1234567"
-                name="SolanaAkk"
-                src="https://s3-alpha.figma.com/hub/file/2204229748/ad1d22a9-445a-4a9d-8ad2-117c2ae9a299-cover.png"
-                wallet="solana"
-                currency="SOL 532"
-                amount="$ 137.24"
-              />
-            </Table.Cell>
-            <Table.Cell>
-              <Wallet
-                alt="wallet avatar"
-                id="1234567"
-                name="PolygonAkk"
-                src="https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/DPYBKVZG55EWFHIK2TVT3HTH7Y.png"
-                wallet="polygon"
-                currency="MATIC 232"
-                amount="$ 206.48"
-              />
-            </Table.Cell> */}
           </Table>
         </>
       }
